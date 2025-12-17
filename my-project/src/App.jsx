c6aeddcb6bb32a339e91da71232da36b734d365a
h676668 <142403603+h676668@users.noreply.github.com>
2025-12-17 17:32:05 +0100

    installert reactrouter

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter as Router} from "rea"
 import './App.css'
 
 function App() {
