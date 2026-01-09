259a53262b56d901a2c544253f956cfff5688e84
h676668 <142403603+h676668@users.noreply.github.com>
2026-01-09 02:53:18 +0100

    .

--- my-project/FrontEnd/src/Components/ChatBot.jsx
+++ my-project/FrontEnd/src/Components/ChatBot.jsx
@@ -31,7 +31,7 @@ const ChatBot = () => {
     setIsLoading(true); // Start animasjon
 
     try {
-      const response = await fetch('https://frisor-ai-service.onrender.com/predict', {
+      const response = await fetch('https://frisor-ai-model.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: textToSend }),
